a84f2789e23709f9607e55697a2e01c0833a7a86
sommersta <sommersta@npchristian.org>
2017-11-01 15:45:59 -0400

    fix sintax errors and move to os.system

--- RapidTools.py
+++ RapidTools.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python
 ##inport things
 import sys
-import subprocess
 import time 
-from goto import with_goto
+import os
 ##done inporting##
-@with_goto
-label .begin
+
 try:
   print (30 * '-')
   f = open('acci.txt', 'r') # 'r' = read
   lines = f.read()
   print lines,
   f.close()
-  print("\nVer 0.1.0")
+  print("\nVer 0.0.1a")
   print ("1. Install Basic tools")
   print ("2. Install adv tools ")
   print ("3. Check apt-get update")
@@ -22,17 +20,17 @@ try:
   if choice == 1:
     print("please wait...")
     time.sleep(5) 
-    subprocess.call('sudo apt-get install gcc+'  )
-    subprocess.call('sudo apt-get install openssl')
-    subprocess.call('sudo apt-get install cowpatty')
-    subprocess.call('sudo apt-get install make')
+    os.system('sudo apt-get install gcc+'  )
+    os.system('sudo apt-get install openssl')
+    os.system('sudo apt-get install cowsay')
+    os.system('sudo apt-get install make')
   elif choice == 2:
         print ("no packges in this menu! yet...")
   elif choice == 3:
-     subprocess.call('sudo apt-get update')      
+      os.system('sudo apt-get update')      
   else:  
         print ("ERROR: submenu not found")
-      goto .begin
+
 #handel errors#
 except IOError as (errno, strerror):
     print "I/O error({0}): {1}".format(errno, strerror)
