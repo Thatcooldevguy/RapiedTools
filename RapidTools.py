a42a97694a586eee071510e5b9a9c5248b5c7d64
sommersta <sommersta@npchristian.org>
2017-11-01 20:39:03 -0400

    adding LOTS of code!

--- RapidTools.py
+++ RapidTools.py
@@ -1,40 +1,68 @@
-#!/usr/bin/python
-##inport things
 import sys
-import time 
-import os
-##done inporting##
+import argparse
+from tools import pip
+from tools import apt
+from tools import git
 
-try:
-  print (30 * '-')
-  f = open('acci.txt', 'r') # 'r' = read
-  lines = f.read()
-  print lines,
-  f.close()
-  print("\nVer 0.0.1a")
-  print ("1. Install Basic tools")
-  print ("2. Install adv tools ")
-  print ("3. Check apt-get update")
-  choice = raw_input('Enter your choice [1-3] : ')
-  choice = int(choice)
-  if choice == 1:
-    print("please wait...")
-    time.sleep(5) 
-    os.system('sudo apt-get install gcc+'  )
-    os.system('sudo apt-get install openssl')
-    os.system('sudo apt-get install cowsay')
-    os.system('sudo apt-get install make')
-  elif choice == 2:
-        print ("no packges in this menu! yet...")
-  elif choice == 3:
-      os.system('sudo apt-get update')      
-  else:  
-        print ("ERROR: submenu not found")
 
-#handel errors#
-except IOError as (errno, strerror):
-    print "I/O error({0}): {1}".format(errno, strerror)
-except ValueError:
-   print "not a menu!"
+def main():
+  args = parse_prog_args()
+  
+  if args.ifile is not None:
+    pass
+  
+  tool_picker(args)
 
-    
+def tool_picker(args):
+    if args.tool == 'pip':
+      pip.handle(args)
+    elif args.tool == 'apt':
+      apt.handle(args)
+    elif args.tool == 'git':
+      git.handle(args)
+    else:
+      print('How did you manage this?!', args.tool)
+      sys.exit(2)
+  
+def parse_prog_args():
+  """
+  This function will take a list of command line arguments given by the user
+  and will format them such that the program can use them in decision making
+  for what functions to use on the given input.for
+  
+  Params: None
+  Return: argparse.Namespace() instance
+  """
+  cli_arg_parser = argparse.ArgumentParser(description='Process command line arguments.')
+  cli_arg_parser.set_defaults(method=None, pkg=None, verbose=False, ifile=None, ofile=None)
+  
+  tool_parser = cli_arg_parser.add_subparsers(title='tool')
+  pip_parser = tool_parser.add_parser('pip')
+  pip_parser.add_argument('-m', choices=['install', 'remove', 'show'], dest='method', required=True)
+  pip_parser.add_argument('pkg')
+  pip_parser.set_defaults(tool='pip')
+
+  apt_parser = tool_parser.add_parser('apt')
+  apt_parser.add_argument('-m', choices=['install', 'remove'], dest='method', required=True)
+  apt_parser.add_argument('pkg')
+  apt_parser.set_defaults(tool='apt')
+  
+  git_parser = tool_parser.add_parser('git')
+  git_parser.add_argument('-m', choices=['clone'], dest='method', required=True)
+  git_parser.add_argument('pkg')
+  git_parser.set_defaults(tool='git')
+  
+  cli_arg_parser.add_argument('-v', action='store_true', dest='verbose')
+  cli_arg_parser.add_argument('-if', dest='ifile')
+  cli_arg_parser.add_argument('-of', dest='ofile')
+  
+  args = cli_arg_parser.parse_args()
+  
+  if args.ifile is not None and args.method is not None or args.ifile is not None and args.pkg is not None:
+    print('Only pass one set of arguments or a file with arguments within.')
+    sys.exit(2)
+  
+  return args
+
+if __name__ == '__main__':
+  main()
